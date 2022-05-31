fb762300a6c24592a7bd3bda1a16b64ca1373ee5
saffan-ahmed-shaikh <saffan20102@gmail.com>
2022-05-31 17:48:13 +0530

    image crop added

--- pages/Components/LogIn.js
+++ pages/Components/LogIn.js
@@ -2,26 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import Router from "next/router";
 import dynamic from "next/dynamic";
-const ImageCropper = dynamic(() => import("../../Components/ImageCropper"), {
-  ssr: false,
-});
+// const ImageCropper = dynamic(() => import("../../Components/ImageCropper"), {
+//   ssr: false,
+// });
+import Demo from "../../Components/ImageCropper";
 export default function Login(props) {
-  //   const [fullname, setfullname] = useState("");
-  const [imageToCrop, setImageToCrop] = useState(undefined);
-  const [croppedImage, setCroppedImage] = useState(undefined);
-  const onUploadFile = (event) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const reader = new FileReader();
-
-      reader.addEventListener("load", () => {
-        const image = reader.result;
-
-        setImageToCrop(image);
-      });
-
-      reader.readAsDataURL(event.target.files[0]);
-    }
-  };
   useEffect(() => {
     const signedInObject = window.localStorage.getItem("signedInObject");
     console.log("signedInObject", JSON.parse(signedInObject));
@@ -30,19 +15,7 @@ export default function Login(props) {
   return (
     <div>
       <Box>
-        <input type="file" accept="image/*" onChange={onUploadFile} />
-        <div>
-          <ImageCropper
-            imageToCrop={imageToCrop}
-            onImageCropped={(croppedImage) => setCroppedImage(croppedImage)}
-          />
-        </div>
-        {croppedImage && (
-          <div>
-            <h2>Cropped Image</h2>
-            <img alt="Cropped Img" src={croppedImage} />
-          </div>
-        )}
+        <Demo />
         <Button onClick={() => Router.push("/Components/SignIn")}>
           Sign In
         </Button>
