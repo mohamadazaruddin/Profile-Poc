fdab92bcb7404441177185d923616f883de8ba63
mohamadazaruddin <azaruddin1307@gmail.com>
2022-06-02 12:09:11 +0530

    Profile-data-integrated

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,21 +1,59 @@
-import React from "react";
-import { Box, Button, FormLabel, Input, Link } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Button, FormLabel, Input, Flex, Image } from "@chakra-ui/react";
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
-import { Router, useRouter } from "next/router";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
-export default function SignIn() {
-  const router = useRouter();
+// const defaultSrc =
+//   "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
+
+export default function SignIn(value) {
+  const [image, setImage] = useState();
+  const [cropData, setCropData] = useState(false);
+  const [cropper, setCropper] = useState();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropper !== "undefined") {
+      setCropData(cropper.getCroppedCanvas().toDataURL());
+      onClose();
+    }
+  };
+
   return (
     <Box p="50px">
       <Box
         w="50%"
+        m="0 auto"
         bg="#3997f8"
         color="#fff"
-        p="30px"
-        mt="10px"
-        margin="auto"
-        borderRadius="10px"
+        p="40px"
+        borderRadius="20px"
         boxShadow="0px 0px 10px #000"
       >
         <Formik
@@ -24,23 +62,29 @@ export default function SignIn() {
             MobNo: "",
             email: "",
             password: "",
+            conPassword: "",
+            cropImg: "",
           }}
           onSubmit={(values) => {
-            localStorage.setItem("signedInObject", JSON.stringify(values));
-            router.push("/Profile");
+            console.log(values, "values");
+            localStorage.setItem("signData", JSON.stringify(values));
+            // localStorage.setItem("ImgData", JSON.stringify(cropData))
           }}
-          validationSchema={Yup.object({
+          validationSchema={Yup.object().shape({
             FullName: Yup.string().max(15, "Too Long").required("Required"),
             MobNo: Yup.string()
-              .max(10, "Number To Long")
-              .min(10, "Number To Short")
+              .max(10, "Invalid Number")
+              .min(10, "Invalid Number")
               .required("Required"),
             email: Yup.string().email("Enter Valid Email").required("Required"),
             password: Yup.string()
               .min(8, "Minimum 8 Characters Required")
               .max(15, "Maximum 15 Characters Required")
               .required("Required"),
-            conPassword: Yup.string(),
+            conPassword: Yup.string().oneOf(
+              [Yup.ref("password"), null],
+              "Passwords must match"
+            ),
           })}
         >
           {({ errors, touched }) => (
@@ -104,7 +148,7 @@ export default function SignIn() {
               </Box>
               <Box py="10px">
                 <FormLabel htmlFor="password" fontSize="20px" fontWeight="600">
-                  Create Password
+                  Password
                 </FormLabel>
                 <Field
                   as={Input}
@@ -120,29 +164,112 @@ export default function SignIn() {
                   </Box>
                 ) : null}
               </Box>
-              <Box textAlign="center" mt="20px">
-                <Button
-                  name="Submit"
-                  type="submit"
-                  bg="#cfba28"
-                  color="black"
-                  w="60%"
-                  _hover={{ bg: "#f3d400" }}
+              <Box py="10px">
+                <FormLabel
+                  htmlFor="conPassword"
+                  fontSize="20px"
+                  fontWeight="600"
                 >
+                  Confirm Password
+                </FormLabel>
+                <Field
+                  as={Input}
+                  name="conPassword"
+                  id="conPassword"
+                  bg="#fff"
+                  color="#333"
+                  type="password"
+                />
+                {errors.conPassword && touched.conPassword ? (
+                  <Box as="p" color="red">
+                    {errors.conPassword}
+                  </Box>
+                ) : null}
+              </Box>
+              <Box py="10px">
+                <Flex>
+                  <Box w="50%">
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                      <ModalOverlay />
+                      <ModalContent>
+                        <ModalHeader textAlign="center">Crop Image</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          <Input
+                            type="file"
+                            name="cropImg"
+                            mb="10px"
+                            onChange={onChange}
+                            bg="transparent"
+                            border="none"
+                            _focus={{ boxShadow: "none" }}
+                          />
+                          <Cropper
+                            style={{ height: 400, width: "100%" }}
+                            zoomTo={0.5}
+                            initialAspectRatio={1}
+                            // preview=".img-preview"
+                            src={image}
+                            viewMode={1}
+                            minCropBoxHeight={10}
+                            minCropBoxWidth={10}
+                            background={false}
+                            responsive={true}
+                            autoCropArea={1}
+                            checkOrientation={false}
+                            onInitialized={(instance) => {
+                              setCropper(instance);
+                            }}
+                            guides={true}
+                          />
+                        </ModalBody>
+                        <ModalFooter>
+                          <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Close
+                          </Button>
+                          <Button onClick={getCropData}>Crop Image</Button>
+                        </ModalFooter>
+                      </ModalContent>
+                    </Modal>
+                    {/* <Input type="image" src={cropData} alt="submit" value={cropData} name="cropImg" /> */}
+                    {cropData ? (
+                      <Image w="100%" src={cropData} alt="cropped" p="20px" />
+                    ) : (
+                      <Box
+                        h="150px"
+                        w="150px"
+                        bg="#c1bebe"
+                        display="grid"
+                        placeItems="center"
+                        color="#000"
+                        m="auto"
+                      >
+                        Image
+                      </Box>
+                    )}
+                  </Box>
+                  <Box pos="relative" w="50%">
+                    <Button
+                      onClick={onOpen}
+                      pos="absolute"
+                      left="50%"
+                      top="50%"
+                      transform="translate(-50%, -50%)"
+                      color="#000"
+                    >
+                      Upload Image
+                    </Button>
+                  </Box>
+                </Flex>
+              </Box>
+              <Box textAlign="center" mt="20px">
+                <Button bg="#FFC803" w="60%" type="submit" name="submit">
                   Submit
                 </Button>
               </Box>
             </Form>
           )}
         </Formik>
-        <Box textAlign="end" mt="25px">
-          <Box>
-            Already have an Account?
-            <Link href="/LogIn" color="#002bff" mx="5px" fontWeight="700">
-              Log in
-            </Link>
-          </Box>
-        </Box>
       </Box>
     </Box>
   );
